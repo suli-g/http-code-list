9f79433d82a67cc7af6927e96fef1555e14d160b
Sulaiman Gafoor <suligzus@gmail.com>
2018-11-15 17:14:33 +0200

    simplified listing command | no customization

---
+++ README.md
@@ -0,0 +1,7 @@
+* HTTP-CODES
+Simple package to list http statuses with their codes
+* Usage:
+```[bash]
+> http-codes
+... codes show up here
+```
--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-const statusMap = require('http-status-codes')
-const swap = require('./lib/swap')
-const stripFlags = require('./lib/strip-flags')
+require('./lib/deep-sort')
+const statusCodes = require('http-status-codes')
+const toNumbers = require('./lib/to-numbers')
+const listCodes = require('./lib/list-codes')
+
 let [,,...args] = process.argv
 
-const codeMap = Object.entries(statusMap).reduce(swap, {})
 if (require.main === module) {
-    let result = codeMap
-    if (args) {
-        let deflagged = stripFlags(args)
-        if (deflagged) {
-            result = deflagged.split(' ').reduce(
-                (stats,_code) => {
-                    _code = _code.toUpperCase()
-                    let [status, code] = [_code, statusMap[_code]]
-                    if (!code) {
-                        ([code, status] = [_code, codeMap[_code]])
-                    }
-                return stats.concat(`${code}: ${status}\n`)
-            }, "")
-        }
-    }
-    console.log(result) 
+    Object.entries(statusCodes)
+        .deepSort()
+        .filter(toNumbers)
+        .forEach(listCodes)
 }
 
-module.exports = {
-    codeMap,
-    statusMap
-}
+module.exports = statusCodes
