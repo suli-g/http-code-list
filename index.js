51ae0bc1c876228d76a9cbb47277fbdcd0afc6e0
Sulaiman Gafoor <suligzus@gmail.com>
2018-11-16 07:10:16 +0200

    add functionality for code/status specification

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+require('./lib/deep-sort')
+const statusCodes = require('http-status-codes')
+const toNumbers = require('./lib/to-numbers')
+const fromArgs = require('./lib/from-args')
+const listCodes = require('./lib/list-codes')
+
+const httpCodes = Object.entries(statusCodes).deepSort()
+let [,,...args] = process.argv
+if (require.main === module) {
+    httpCodes
+    .filter(toNumbers)
+    .filter(fromArgs(args))
+    .forEach(listCodes)
+}
+else module.exports = {httpCodes, args}
